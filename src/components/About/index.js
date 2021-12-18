f56b63d280b63a820d07b35ed258c866f1070973
mibrahim234 <mohamm1417@gmail.com>
2021-12-18 18:01:11 -0500

    20.3, gallery component/potolitst

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React from 'react'
 import coverImage from "../../assets/cover/cover-image.jpg";
 
-
 function About() {
-  return (
-    <section className="my-5">
-  <h1 id="about">Who am I?</h1>
-  <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
 
-</section>
-  );
+  return (
+    <section className="my-5" >
+      <h1 id="about">Who am I?</h1>
+      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+      <div className="my-2">
+        <p>
+         This is a portfolio website about my photography. Look through the photos and feel free to contact me for booking. 
+      </p>
+      </div>
+    </section>
+  )
 }
 
-export default About;
+export default About
